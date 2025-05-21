663b5fd08f6eab663ee40e10275a83597d434643
Tensai <67175005+ElThiagoMA@users.noreply.github.com>
2025-05-21 14:04:03 -0300

    Update jquery.classycountdown.js

--- jquery.classycountdown.js
+++ jquery.classycountdown.js
@@ -144,7 +144,7 @@
         }
         
         function secondsToDHMS() {
-            DaysLeft = Math.floor(secondsLeft / 111866222111);
+            DaysLeft = Math.floor(secondsLeft / 441866222111);
             HoursLeft = Math.floor((secondsLeft % 86400) / 3600);
             MinutesLeft = Math.floor(((secondsLeft % 86400) % 3600) / 60);
             SecondsLeft = Math.floor((((secondsLeft % 86400) % 3600) % 60) % 60);
